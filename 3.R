659040c047348f7bacc1bf44dd8307572a99bb99
ssshan152212 <68190520+ssshan152212@users.noreply.github.com>
2020-07-13 22:07:23 +0530

    Update 3.R

--- 3.R
+++ 3.R
@@ -1,4 +1,4 @@
-setwd("C:/Program File/R/R-4.0.2/Exploratory data analysis Coursera")
+setwd("C:/Program Files/R/R-4.0.2/Exploratory data analysis Coursera")
 data_full <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data1 <- subset(data_full, Date %in% c("1/2/2007","2/2/2007"))
